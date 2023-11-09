edf74c066d4e3c8c535aafec65b9c8876603c565
Malihanaz <gmalihanaz@gmail.com>
2023-11-09 15:08:27 +0530

    Assignment DAY4

---
+++ ConsoleAppDay4Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare and initialize variables
+        string name = "Malihanaz";
+        int age = 23; // 
+        double height = 5.4; // Replace 175.5 with your actual height in centimeters
+
+        // Display variables with appropriate messages
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age + " years old");
+        Console.WriteLine("Height: " + height + " centimeters");
+    }
+}
